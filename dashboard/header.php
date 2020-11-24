5c9863a172fce575b4b0f97263b2728abfbca19c
unknown <LokiGodofBattle@gmx.de>
2020-11-24 12:12:02 +0100

    first commit

---
+++ dashboard/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
+
+    <title>Transponder System</title>
+	<link rel="icon" type="image/png" href="favicon.png">
+
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Transponder verleihen</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="awardedTransponder.php">Ausgeliehende Transponder</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <a href="#" class="btn btn-outline-warning my-2 my-sm-0" role="button" aria-pressed="true">Pförtner Logout</a>
+      </form>
+    </div>
+    </nav>
---
+++ dashboard/index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+include_once ('header.php');
+
+include_once('functions.php');
+?>
+
+<h2> Transponder verleihen: </h2>
+<form method="post" class="form-group form-control-lg ">
+  <div class="form-group row">
+    <label for="inputroom" class="col-sm-1 col-form-label">Raum:</label>
+    <div class="col-sm-10">
+      <input type="room" class="form-control-lg" name="inputroom" placeholder="0.401">
+    </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="inputmatnr" class="col-sm-1 col-form-label">Matrikelnummer:</label>
+    <div class="col-sm-10">
+      <input type="matnr" class="form-control-lg" name="inputmatnr" placeholder="11132500">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputdate" class="col-sm-1 col-form-label">Zeitraum:</label>
+    <div class="col-sm-10">
+      <input type="von" class="form-control-lg" name="inputvon" placeholder="von">
+      <input type="bis" class="form-control-lg" name="inputbis" placeholder="bis">
+    </div>
+  </div>
+  <button formaction="ausleihen.php" type="submit"  class="btn btn-primary mb-3">Transponder ausleihen</button>
+  <button formaction="berechtigung.php" type="submit"  class="btn btn-primary mb-3">Berechtigungen überprüfen</button>
+
+</form>
+
+
+
+
+
+<?php
+ include_once ('footer.php');
+?>
