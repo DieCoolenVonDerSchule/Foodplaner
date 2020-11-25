f910e68510f943b51fe33b191b8bd18a0de431de
DieCoolenVonDerSchule <DieCoolenVonDerSchule@gmx.de>
2020-11-25 14:08:45 +0100

    working on index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,34 +4,40 @@ session_start();
 include_once ('header.php');
 
 include_once('functions.php');
+
+$pdo = new PDO('mysql:host=localhost;dbname=foodplaner', 'root', '');
+
+$sql='SELECT recipe_name, calender_entry_date FROM calender_entry JOIN recipes ON (calender_entry.recipe_id = recipes.recipe_id) WHERE calender_entry_date > CURRENT_TIMESTAMP AND calender_entry_date < DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 7 DAY)';
+
 ?>
 
 <h2> Kalender </h2>
 <form method="post" class="form-group form-control-lg ">
-  <div class="form-group row">
-    <label for="inputroom" class="col-sm-1 col-form-label">Raum:</label>
-    <div class="col-sm-10">
-      <input type="room" class="form-control-lg" name="inputroom" placeholder="0.401">
-    </div>
-  </div>
+  
 
-  <div class="form-group row">
-    <label for="inputmatnr" class="col-sm-1 col-form-label">Matrikelnummer:</label>
-    <div class="col-sm-10">
-      <input type="matnr" class="form-control-lg" name="inputmatnr" placeholder="11132500">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputdate" class="col-sm-1 col-form-label">Zeitraum:</label>
-    <div class="col-sm-10">
-      <input type="von" class="form-control-lg" name="inputvon" placeholder="von">
-      <input type="bis" class="form-control-lg" name="inputbis" placeholder="bis">
-    </div>
-  </div>
+<table class="table table-striped table-bordered" style="width:100%">
 
+<thead>
+  <tr>
+    <td>Rezept Name</td>
+  </tr>
+  </thead>
 
+  <tbody>
+<?php
 
+$con = getConnection();
+$query = mysqli_query($con, $sql);
 
+while ($data = mysqli_fetch_array($query)) { ?>
+
+          <tr>
+            <td><?php echo $data['recipe_name']; ?></td>
+          </tr>
+      <?php } ?>
+      </tbody>
+
+  </table>
 
 
   <button formaction="recipe.php" type="submit"  class="btn btn-primary mb-3">Gericht hinzufügen</button>
