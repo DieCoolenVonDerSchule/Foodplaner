1bcc08a28687b6c1ffaa8e3afc744ad862622b7a
Marc Kevin Zenzen <marc_kevin.zenzen@smail.th-koeln.de>
2020-11-25 14:08:24 +0100

    Rezepte Seite angelegt

--- dashboard/functions.php
+++ dashboard/functions.php
@@ -12,19 +12,19 @@ $connection=mysqli_connect($mysqlhost, $mysqluser, $mysqlpwd, $mysqldb)
 }
 
 
-
-function getAllawardedTransponder(){
+function getRecipes() {
   $con = getConnection();
-  $sql = 'SELECT PT.transId, transNr, personName, von, bis FROM PERSONEN_TRANSPONDER PT JOIN PERSONEN P ON (P.personId=PT.personId) JOIN TRANSPONDER T ON (T.transId=PT.transId);';
+  $sql = 'SELECT * FROM recipes';
   $query = mysqli_query($con,$sql);
   return $query;
 }
 
-function getSearchedRoom() {
-  $con = getConnection();
-  $sql = 'SELECT * FROM ausleihe';
-  $query = mysqli_query($con,$sql);
-  return $query;
+function getRecipeByID($id){
+    $con = getConnection();
+    $sql = "SELECT * FROM recipes WHERE recipe_id = $id";
+    $query = mysqli_query($con, $sql);
+    $data = mysqli_fetch_array($query);
+    return $data;
 }
 
 ?>
